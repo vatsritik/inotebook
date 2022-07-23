e3a9473f6d89ef7ef00e98b05120abf03dc7ee76
vatsritik <49120743+vatsritik@users.noreply.github.com>
2022-07-23 06:19:26 -0700

    Update AddNotes.js

--- src/Components/AddNotes.js
+++ src/Components/AddNotes.js
@@ -4,7 +4,7 @@ import noteContext from '../context/notes/noteContext';
 const AddNotes = () => {
 
     const context=useContext(noteContext);
-    const {notes,addNote}=context;
+    const {addNote}=context;
     const[note,setNote]=useState({title:"",description:"",tag:""})
 
     const handleClick=(e)=>{
